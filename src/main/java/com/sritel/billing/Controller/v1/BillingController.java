3239dcdf3724c5dcf08129836a406c41a06c8c9d
Buddhika Senanayake <bnsbuddhika@gmail.com>
2024-09-28 23:53:33 +0530

    Final changes

--- src/main/java/com/sritel/billing/Controller/v1/BillingController.java
+++ src/main/java/com/sritel/billing/Controller/v1/BillingController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/billing")
+@CrossOrigin
 public class BillingController {
 
     @Autowired
--- src/main/java/com/sritel/billing/Controller/v1/PaymentController.java
+++ src/main/java/com/sritel/billing/Controller/v1/PaymentController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/payment")
+@CrossOrigin
 public class PaymentController {
 
     @Autowired
